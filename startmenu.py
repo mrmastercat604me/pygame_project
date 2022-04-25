6a61b31bff9d8b35bb2184fbb2a8c8abf6fdb60c
ELLIOTT <9574344@graniteschools.org>
2022-04-25 10:14:47 -0600

    just added screen with main menu kinda

--- startmenu.py
+++ startmenu.py
@@ -1,1 +1,34 @@
+import pygame, sys
 
+mainClock = pygame.time.Clock()
+from pygame.locals import *
+pygame.init()
+pygame.display.set_caption('game base')
+screen = pygame.display.set_mode((500,500),0,32)
+
+font = pygame.font.SysFont(None, 20)
+
+def draw_text(text, font, color, surface, x,y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+def main_menu():
+    while True:
+
+        screen.fill((0,0,0))
+        draw_text('main menu',font, (255, 255, 255), screen, 20, 20)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+        mainClock.tick(60)
+
+main_menu()

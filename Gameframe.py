1cc10432e54d50a1cb593fc1f388c2b4723addac
BARNETT <9718085@graniteschools.org>
2022-04-29 10:33:51 -0600

    jo mamma

--- Gameframe.py
+++ Gameframe.py
@@ -1,12 +1,13 @@
 import pygame
 import random
+import math
 pygame.init()
 
 
 width = 800
 height = 800
 fps = 60
-Velocity = 15
+Velocity = 10
 display_surface = pygame.display.set_mode((width, height))
 Backgroundimage = pygame.image.load("assets/background-black.png")
 Backgroundimage_rec= Backgroundimage.get_rect()
@@ -18,7 +19,16 @@ enemyimage = pygame.image.load("assets/meteor1.png")
 enemyimage_rec= enemyimage.get_rect()
 enemy_x_pos = random.randint(-100,100) 
 enemy_y_pos = random.randint(-100,100)
+def face_mouse(image,image_rect,correction_angle,surface):
+    mx, my = pygame.mouse.get_pos()
+    dx,dy =  mx - image_rect.centerx, my - image_rect.centery
+    angle =  math.degrees(math.atan2(-dy, dx)) - correction_angle
+    rot_image = pygame.transform.rotate(image,angle)
+    rot_image_rect = rot_image.get_rect(center = image_rect.center)
+    surface.blit(rot_image,rot_image_rect.topleft)
+    pygame.display.update()
 
+       
 
 Backgroundimage_rec.topleft=(0,0)
 clock=pygame.time.Clock()
@@ -30,6 +40,8 @@ shipimage_rect.centerx = width//2
 shipimage_rect.centery = height//2
 enemyimage_rec.y = enemy_y_pos
 enemyimage_rec.x = enemy_x_pos
+pygame.display.set_caption('blastroids')
+font = pygame.font.Font('freesansbold.ttf', 32)
 if __name__ == "__main__":
         running = True
 while running:
@@ -52,16 +64,16 @@ while running:
                 running = False
         if event.type == pygame.QUIT:
             running = False
-
+        
         display_surface.fill((0,0,0))
+        
         display_surface.blit(Backgroundimage,Backgroundimage_rec)
         if surface is not None:
                 display_surface.blit(surface, (0, 0))
         display_surface.blit(enemyimage,enemyimage_rec)
-        display_surface.blit(shipimage,shipimage_rect)
+        #display_surface.blit(shipimage,shipimage_rect)
+        face_mouse(shipimage, shipimage_rect, 90, display_surface)
         pygame.display.update()
-        if running == False:
-                pygame.quit
-pygame.display.update(Backgroundimage_rec)
-clock.tick(fps)
+        pygame.display.update(Backgroundimage_rec)
+        clock.tick(fps)
 pygame.quit()

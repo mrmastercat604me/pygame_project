8444888c26a3242c42e084ed642c3efc0105ebcb
ELLIOTT <9574344@graniteschools.org>
2022-05-04 09:11:26 -0600

    fiex positioning and backround on cheats

--- options.py
+++ options.py
@@ -156,28 +156,23 @@ def options():
 
 def cheat():
     running = True
-    screen.fill((0,0,0))
     font = pygame.font.Font(None, 32)
     clock = pygame.time.Clock()
-    input_box = pygame.Rect(100, 100, 140, 32)
+    input_box = pygame.Rect(300, 300, 150, 32)
     color_inactive = pygame.Color('lightskyblue3')
     color_active = pygame.Color('dodgerblue2')
     color = color_inactive
     active = False
     text = ''
     while running:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                # If the user clicked on the input_box rect.
                 if input_box.collidepoint(event.pos):
-                    # Toggle the active variable.
                     active = not active
                 else:
                     active = False
-                # Change the current color of the input box.
                 color = color_active if active else color_inactive
             if event.type == pygame.KEYDOWN:
                 if active:
@@ -191,15 +186,12 @@ def cheat():
                 if event.key == K_ESCAPE:
                     running = False
 
-        screen.fill((30, 30, 30))
-        # Render the current text.
+        screen.fill((0,0,0))
+        screen.blit(Backgroundimage,Backgroundimage_rec)
         txt_surface = font.render(text, True, color)
-        # Resize the box if the text is too long.
         width = max(200, txt_surface.get_width()+10)
         input_box.w = width
-        # Blit the text.
         screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
-        # Blit the input_box rect.
         pygame.draw.rect(screen, color, input_box, 2)
 
         pygame.display.update()

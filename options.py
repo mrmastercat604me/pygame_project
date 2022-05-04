f129b4cbd0c40cc3ac15bac0935b39fd6f458537
ELLIOTT <9574344@graniteschools.org>
2022-05-04 09:02:27 -0600

    added a start to cheat codes

--- options.py
+++ options.py
@@ -118,36 +118,106 @@ def game():
 
 def options():
     running = True
-    while True:
+    click = False
+    while running:
         screen.fill((0,0,0))
         screen.blit(Backgroundimage,Backgroundimage_rec)
         draw_text('Options',font, (255, 0, 0), screen, 290, 100)
-        #----------------------------------
+        mx, my = pygame.mouse.get_pos()
+        button_3 = pygame.Rect(190, 300, 400, 70)
+        button_4 = pygame.Rect(190, 400, 400, 70)
+        button_3.centerx = 400
+        button_4.centerx = 400
+        pygame.draw.rect(screen, (150, 0, 30), button_3)
+        pygame.draw.rect(screen, (150, 0, 30), button_4)
+        draw_text('Cheat Codes',font, (0, 0, 0), screen, 240, 310)
+        draw_text('Colors',font, (0, 0, 0), screen, 305, 410)
+        if button_3.collidepoint((mx, my)):
+            if click:
+                cheat()
+        if button_4.collidepoint((mx, my)):
+            if click:
+                colors()
+        click = False
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
-                    main_menu()
+                    running = False
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     click = True
-        #--------------------------------------
-        mx, my = pygame.mouse.get_pos()
-        button_1 = pygame.Rect(190, 300, 400, 70)
-        button_2 = pygame.Rect(190, 400, 400, 70)
-        button_1.centerx = 400
-        button_2.centerx = 400
-        pygame.draw.rect(screen, (150, 0, 30), button_1)
-        pygame.draw.rect(screen, (150, 0, 30), button_2)
-        draw_text('Cheat Codes',font, (0, 0, 0), screen, 240, 310)
-        draw_text('Volume',font, (0, 0, 0), screen, 305, 410)
-        #if button_1.collidepoint((mx, my)):
-            #if click:()
-        #if button_2.collidepoint((mx, my)):
-            #if click:()
-        #----------------------------------
+
+        pygame.display.update()
+        mainClock.tick(60)
+    
+
+def cheat():
+    running = True
+    screen.fill((0,0,0))
+    font = pygame.font.Font(None, 32)
+    clock = pygame.time.Clock()
+    input_box = pygame.Rect(100, 100, 140, 32)
+    color_inactive = pygame.Color('lightskyblue3')
+    color_active = pygame.Color('dodgerblue2')
+    color = color_inactive
+    active = False
+    text = ''
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # If the user clicked on the input_box rect.
+                if input_box.collidepoint(event.pos):
+                    # Toggle the active variable.
+                    active = not active
+                else:
+                    active = False
+                # Change the current color of the input box.
+                color = color_active if active else color_inactive
+            if event.type == pygame.KEYDOWN:
+                if active:
+                    if event.key == pygame.K_RETURN:
+                        print(text)
+                        text = ''
+                    elif event.key == pygame.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += event.unicode
+                if event.key == K_ESCAPE:
+                    running = False
+
+        screen.fill((30, 30, 30))
+        # Render the current text.
+        txt_surface = font.render(text, True, color)
+        # Resize the box if the text is too long.
+        width = max(200, txt_surface.get_width()+10)
+        input_box.w = width
+        # Blit the text.
+        screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
+        # Blit the input_box rect.
+        pygame.draw.rect(screen, color, input_box, 2)
+
+        pygame.display.update()
+        mainClock.tick(60)
+
+def colors():
+    running = True
+    while running:
+        screen.fill((0,0,0))
+
+        draw_text('colors',font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
 
         pygame.display.update()
         mainClock.tick(60)

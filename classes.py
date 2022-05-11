f4e55d1cb3abf3a42e85dde2eb54246a2aa09769
Ethan <mrmastercat604@gmail.com>
2022-05-11 08:21:25 -0600

    Meteors.

--- classes.py
+++ classes.py
@@ -6,17 +6,19 @@ class Player():
         self.image = pygame.image.load(f'assets/pixel_ship_{self.color}.png')
         self.surface = surface
         self.velocity = 7
-        self.click = False
         self.lives = 3
         self.status = "alive"
+        self.click = False
         self.score = 0
         self.rect = self.image.get_rect()
         self.rect.center = pos
-        self.laser = Laser(self.color,(-100,-100),self.surface)
+        self.laser = None
+        self.angle = 0
 
 
-    def controls(self):
-        for event in pygame.event.get():
+    def controls(self,events):
+        click = False
+        for event in events:
             key = pygame.key.get_pressed()
             if key[pygame.K_w]:
                 self.rect.y -= self.velocity
@@ -26,42 +28,47 @@ class Player():
                 self.rect.y += self.velocity
             if key[pygame.K_d]:
                 self.rect.x += self.velocity
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    self.click = True
-            # if event.type == pygame.MOUSEBUTTONUP:
-            #     if event.button == 1:
-            #         self.click = False
             if event.type == pygame.QUIT:
                 pygame.quit()
             if key[pygame.K_ESCAPE]:
                 self.status = "dead"
-        if self.click:
-            self.laser = Laser(self.color,self.rect.center,self.surface)
-            print("PEW")
-        self.click = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    self.click = True
 
     def render(self,correction_angle):
             mx, my = pygame.mouse.get_pos()
             dx,dy =  mx - self.rect.centerx, my - self.rect.centery
-            angle =  math.degrees(math.atan2(-dy, dx)) - correction_angle
-            rot_image = pygame.transform.rotate(self.image,angle)
+            self.angle =  math.degrees(math.atan2(-dy, dx)) - correction_angle
+            rot_image = pygame.transform.rotate(self.image,self.angle)
             rot_image_rect = rot_image.get_rect(center = self.rect.center)
             #DEBUG pygame.draw.rect(surface,(255,255,255),rot_image_rect,2)
             self.surface.blit(rot_image,rot_image_rect.topleft)
-            self.laser.run()
+            if self.laser is not None:
+                self.laser.run()
 
     def collision(self):
         pass
 
-    def status(self):
+    def get_status(self):
         if self.lives <= 0:
             self.status = "dead"
-        
-    
-    def run(self):
-        self.controls()
+        else:
+            self.status = "alive"
+
+    def summon_laser(self):
+        if self.click:
+            self.laser = Laser(self.color,self.rect.center,self.surface)
+            self.laser.degrees = self.angle
+            self.laser.image = pygame.transform.rotate(self.laser.image,self.laser.degrees)
+            self.laser.rect = self.laser.image.get_rect(center = self.laser.rect.center)
+        self.click = False
+
+    def run(self,events):
+        self.controls(events)
         self.collision()
+        self.get_status()
+        self.summon_laser()
         self.render(90)
 
 
@@ -74,35 +81,46 @@ class Laser():
         self.click = False
         self.rect = self.image.get_rect()
         self.rect.center = pos
+        self.degrees = 0
 
         self.base = pygame.image.load(f'assets/pixel_laser_{self.color}.png')
         self.base_rect = self.base.get_rect()
         self.base_rect.center = (-100,-100)
     
-    def face_mouse(self,correction_angle):
-        mx, my = pygame.mouse.get_pos()
-        dx,dy =  mx - self.rect.centerx, my - self.rect.centery
-        angle =  math.degrees(math.atan2(-dy, dx)) - correction_angle
-        rot_image = pygame.transform.rotate(self.image,angle)
-        rot_image_rect = rot_image.get_rect(center = self.rect.center)
-        #DEBUG pygame.draw.rect(surface,(255,255,255),rot_image_rect,2)
-        self.surface.blit(rot_image,rot_image_rect.topleft)
+    def render(self):
+        self.surface.blit(self.image,self.rect)
 
 
     def collision(self):
         pass
     
     def run(self):
-        self.face_mouse(90)
+        self.render()
         self.collision()
+
 
 class Meteor():
     def __init__(self,level,pos,surface):
-        self.image = pygame.image.load(f'assets/meteor{level}')
+        self.image = pygame.image.load(f'assets/meteor{level}.png')
         self.surface = surface
         self.level = level
         self.rect = self.image.get_rect()
         self.rect.center = pos
 
-    def collision(self):
-        pass
+    def collision(self,rect):
+        if self.rect.x >= 800:
+            pass
+        elif self.rect.x <= 0:
+            pass
+        elif self.rect.y >= 800:
+            pass
+        elif self.rect.y <= 0:
+            pass
+        else:
+            pass
+        if self.rect.colliderect(rect):
+           return True
+        return False
+    def render(self):
+        self.surface.blit(self.image,self.rect)
+
--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import random,math,sys
+from classes import Meteor
 
 
 def draw_text(text, font, color, surface, x,y):
@@ -11,13 +12,16 @@ def draw_text(text, font, color, surface, x,y):
 def game(surface,player,clock,background,background_rect):
 
     running = True
-    enemyimage = pygame.image.load("assets/meteor1.png")
-    enemyimage_rect= enemyimage.get_rect()
-    enemyimage_rect.x = random.randint(20,780) 
-    enemyimage_rect.y = random.randint(20,780)
-    if enemyimage_rect.colliderect(player.rect):
-        enemyimage_rect.x = random.randint(20,780) 
-        enemyimage_rect.y = random.randint(20,780)
+    #enemyimage = pygame.image.load("assets/meteor1.png")
+    # enemyimage_rect= enemyimage.get_rect()
+    # enemyimage_rect.x = random.randint(20,780) 
+    # enemyimage_rect.y = random.randint(20,780)
+    # if enemyimage_rect.colliderect(player.rect):
+    #     enemyimage_rect.x = random.randint(20,780) 
+    #     enemyimage_rect.y = random.randint(20,780)
+    meteors = []
+    meteor = Meteor(1,(100,100),surface)
+    meteors.append(meteor)
     
 
     laser = pygame.image.load(f'assets/pixel_laser_{player.color}.png')
@@ -28,28 +32,36 @@ def game(surface,player,clock,background,background_rect):
     laser_first_rect.center = (-100,-100)
 
     surface.fill((0,0,0))
+
     while running:
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
+                if event.key == pygame.K_m:
+                    meteor = Meteor(1,(random.randint(0,800),random.randint(0,800)),surface)
+                    meteors.append(meteor)
         if player.status == "dead":
             running = False
 
-
-        #if enemyimage_rect.colliderect(player.rect):
-        #    lives -= 1
-       #     enemyimage_rect.x = random.randint(20,780) 
-        #    enemyimage_rect.y = random.randint(20,780)
+        
 
 
+        # if enemyimage_rect.colliderect(player.rect):
+        #     player.lives -= 1
+        #     enemyimage_rect.x = random.randint(20,780) 
+        #     enemyimage_rect.y = random.randint(20,780)
+        #     if enemyimage_rect.colliderect(player.rect):
+        #         enemyimage_rect.x = random.randint(20,780) 
+        #         enemyimage_rect.y = random.randint(20,780)
 
-        if laser_rect.colliderect(enemyimage_rect):
-            enemyimage_rect.x = random.randint(20,780)
-            enemyimage_rect.y = random.randint(20,780)
+        # if laser_rect.colliderect(enemyimage_rect):
+        #     enemyimage_rect.x = random.randint(20,780)
+        #     enemyimage_rect.y = random.randint(20,780)
 
 
 
@@ -58,11 +70,15 @@ def game(surface,player,clock,background,background_rect):
         font = pygame.font.SysFont(None, 40)
         draw_text((f"Lives: {player.lives}"),font,(255,0,0),surface,0,0)
         draw_text((f"Score: {player.score}"),font,(255,255,0),surface,690,0)
-        font = pygame.font.SysFont(None, 75)
 
-        player.run()
-        surface.blit(enemyimage,enemyimage_rect)
+        #surface.blit(enemyimage,enemyimage_rect)
+        player.run(events)
+        for meteor in meteors:
+            meteor.render()
+            if player.laser is not None:
+                if meteor.collision(player.laser.rect):
+                    meteors.remove(meteor)
+        
 
-        surface.blit(laser,laser_rect)
         pygame.display.update()
         clock.tick(60)
